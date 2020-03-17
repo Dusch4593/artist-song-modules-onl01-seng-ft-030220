6fa799e3e8a587bb562c7c690b2ac34f24dc98a1
Brandon Dusch <brandondusch@gmail.com>
2020-03-17 20:15:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Artist
-  extend Memorable, Findable
+  extend Memorable, Findable, Paramable
   attr_accessor :name
   attr_reader :songs
 
@@ -31,9 +31,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
